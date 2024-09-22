d70ab17304b561efafb86b0b83ce3063545f1cec
MonetteParangat <monette.parangat@ibm.com>
2024-09-22 21:04:56 +0800

    added entity and connection to database

--- banking-application/src/main/java/com/bank/bankingApplication/BankingApplication.java
+++ banking-application/src/main/java/com/bank/bankingApplication/BankingApplication.java
@@ -1,4 +1,4 @@
-package com.bank.banking_application;
+package com.bank.bankingApplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
